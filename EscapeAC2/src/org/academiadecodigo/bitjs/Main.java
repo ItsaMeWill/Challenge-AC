a39846727d67b6bc817b0edc82e2cceee9b9258d
catarinacandido <cgmartinscandido@gmail.com>
2020-10-14 18:55:01 +0000

    questions working

--- EscapeAC2/src/org/academiadecodigo/bitjs/Main.java
+++ EscapeAC2/src/org/academiadecodigo/bitjs/Main.java
@@ -1,17 +1,25 @@
 package org.academiadecodigo.bitjs;
 
+import org.academiadecodigo.bitjs.enemy.Question;
+import org.academiadecodigo.bitjs.enemy.Ricardo;
 import org.academiadecodigo.bitjs.player.Game;
 import org.academiadecodigo.bitjs.rooms.MacRoom;
 import org.academiadecodigo.bitjs.player.Player;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        Game game1 = new Game();
-        game1.startLevel1();
 
+        Game game1 = new Game();
+       //game1.startLevel1();
+
+Ricardo ri = new Ricardo();
+ri.makeQuestion(Question.QUESTION1);
 
 
 
--- EscapeAC2/src/org/academiadecodigo/bitjs/enemy/MCS.java
+++ EscapeAC2/src/org/academiadecodigo/bitjs/enemy/MCS.java
@@ -9,38 +9,42 @@ abstract public class MCS {
 
     private Picture face;
     private Rectangle rectangle;
-    private Question question;
+
 
     public MCS(Picture face, Rectangle rectangle){
         this.face = face;
         this.rectangle = rectangle;
-    }
-
-    public void makeQuestion(Question question, Question correct) {
-
-        //create the question screen and write the question
-
-        Rectangle questionScreen = new Rectangle(10, 10, 500, 500);
-        questionScreen.fill();
-        Text questionMc = new Text(10, 10, question.getQuestion());
-        questionMc.setColor(Color.WHITE);
-        questionMc.draw();
-
-        //write the answers
-
-        Text answer1 = new Text(100, 100, question.getAnswer1());
-        Text answer2 = new Text(20, 120, question.getAnswer2());
-        Text answer3 = new Text(20, 140, question.getAnswer3());
-        answer1.setColor(Color.WHITE);
-        answer2.setColor(Color.WHITE);
-        answer3.setColor(Color.WHITE);
-        answer1.grow(10,10);
-
-        answer1.draw();
-        answer2.draw();
-        answer3.draw();
 
     }
+
+
+    public void makeQuestion(Question question,Text ans1,Text ans2,Text ans3) {
+    //create the question screen and write the question
+
+    Rectangle questionScreen = new Rectangle(10, 10, 500, 500);
+            questionScreen.setColor(Color.BLUE);
+            questionScreen.fill();
+    Text questionMc = new Text(10, 10, question.getQuestion());
+            questionMc.setColor(Color.WHITE);
+            questionMc.draw();
+
+    //write the answers
+
+    //Text answer1 = new Text(100, 100, question.getAnswer1());
+    //Text answer2 = new Text(20, 120, question.getAnswer2());
+    //Text answer3 = new Text(20, 140, question.getAnswer3());
+            ans1.setColor(Color.WHITE);
+            ans2.setColor(Color.WHITE);
+            ans3.setColor(Color.WHITE);
+            ans1.grow(10,10);
+
+            ans1.draw();
+            ans2.draw();
+            ans3.draw();
+
+}
+
+
 
     public Picture getFace() {
         return face;
@@ -50,7 +54,5 @@ abstract public class MCS {
         return rectangle;
     }
 
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
+
 }
